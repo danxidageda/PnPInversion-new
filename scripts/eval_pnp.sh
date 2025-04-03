ba3f1493574f3f7ea462e2a780187a1f2a8a6084
danxidageda <2559249495@qq.com>
2025-04-03 12:04:12 +0800

    updata: 脚本更新

--- scripts/eval_pnp.sh
+++ scripts/eval_pnp.sh
@@ -2,7 +2,7 @@ python run_editing_pnp.py \
 	--model_path /data/lyw/stable-diffusion-v1-4 \
 	--eval-datasets EditEval_v1 \
 	--height 1024 \
-	--width 102 \
+	--width 1024 \
 	--num-step 30 \
   --guidance 7.5 \
   --output_dir pnp_output \
